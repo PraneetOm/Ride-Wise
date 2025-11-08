fbc1fab9c1c304d608c8670d883159792618c6e8
Praneet Om <om.praneet.in@gmail.com>
2025-11-08 19:52:14 +0530

    Fix API base URL in api.js

--- src/api.js
+++ src/api.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_BASE || "https://ride-wise.onrender.com/api";
+const API_BASE = import.meta.env.VITE_API_BASE || "https://ride-wise.onrender.com";
 
 export default axios.create({
   baseURL: API_BASE + "/api",
   headers: { "Content-Type": "application/json" }
 
 });
+
