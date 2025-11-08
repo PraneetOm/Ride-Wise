226d8729aec3344f5532b25ba920c9f4c6e9406c
Praneet Om <om.praneet.in@gmail.com>
2025-11-08 19:51:22 +0530

    Update API base URL to production environment

--- src/api.js
+++ src/api.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000";
+const API_BASE = import.meta.env.VITE_API_BASE || "https://ride-wise.onrender.com/api";
 
 export default axios.create({
   baseURL: API_BASE + "/api",
   headers: { "Content-Type": "application/json" }
+
 });
